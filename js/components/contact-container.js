e3a720d4daded06701c84eb892a8f58eb05f76c2
davellaj <jamie.davella@gmail.com>
2016-12-08 12:49:06 -0500

    add link routes as well as passed down CONTACTS instead of import them in contact-container

--- js/components/contact-container.js
+++ js/components/contact-container.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import CONTACTS from '../contacts';
+//import CONTACTS from '../contacts';
 import Contact from  './contact';
 
 export default function ContactContainer(props) {
-    const contact = CONTACTS[props.params.contactId];
+	console.log(props)
+	let contactRoute = props.route.CONTACTS;
+    const contact = contactRoute[props.params.contactId];
     return <Contact id={contact.id} name={contact.name}
                     phoneNumber={contact.phoneNumber} />;
 };
--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 
+import CONTACTS from './contacts'
+
 import ContactListContainer from './components/contact-list-container';
 import ContactContainer from './components/contact-container'
 
@@ -13,7 +15,7 @@ const routes = (
   <Router history={hashHistory}>
     <Route path="/contacts" component={App}>
     	<IndexRoute component={ContactListContainer} /> 
-    	<Route path=":contactId" component={ContactContainer} />
+    	<Route path=":contactId" component={ContactContainer} CONTACTS={CONTACTS}/>
     </Route>
   </Router>
 );
